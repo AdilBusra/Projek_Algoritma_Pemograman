73578e5e816ed79ea8f449341291258499e5d154
leonardauguste <leonardaugustee@gmail.com>
2025-05-14 09:48:56 +0700

    Update and rename file_3_4.cpp to file_3.cpp

---
+++ file_3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    system ("cls");
+    int jumlahOrang, jumlahMalam;
+    
+    cout << "\nBerapa malam Anda menginap? ";
+    cin >> jumlahMalam;
+    cout << "Untuk berapa orang? ";
+    cin >> jumlahOrang;
+
+}
+
+    
